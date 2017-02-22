707a09871aac727ed65e95a9ec99073487ee89d1
Cristian Castro <cristiancastrodc@gmail.com>
2017-02-21 20:55:11 -0500

    Copyright logo

--- wp-content/themes/devdmbootstrap3_peruterraexpeditions/footer.php
+++ wp-content/themes/devdmbootstrap3_peruterraexpeditions/footer.php
@@ -1,3 +1,7 @@
+  <!-- Copyright Section -->
+  <div id="copyright">
+    <a href="http://insidelab.net" target="_blank"></a>
+  </div>
   <?php get_template_part('template-part', 'footer'); ?>
   <?php wp_footer(); ?>
 </body>
